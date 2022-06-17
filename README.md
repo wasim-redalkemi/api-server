25a422134c49cea0506b5156987a9886ac78655c
wasim-redalkemi <wasim.redalkemi@gmail.com>
2022-06-17 11:32:44 +0530

    upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Getting Started with Node App
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3001](http://localhost:3001) to view it in your browser.
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const request = require('request');
+var cors = require('cors');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+// use it before all route definitions
+app.use(cors({origin: '*'}));
+
+app.get(["/posts", "/categories"], (req, res) => {  
+  // Request URL
+  var url = 'https://themoviesverse.co/wp-json/wp/v2'+req.url;
+  request(url, (error, response, body) => { 
+    //console.log(response.headers['x-wp-totalpages']);
+    if(error){
+      result = error;
+    }
+    else{
+      result = body;
+      res.set('totalPages', response.headers['x-wp-totalpages']);
+    }
+    res.json(JSON.parse(result));
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
