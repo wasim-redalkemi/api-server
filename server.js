f84b79e7feac9829011c58f70a903ee872522022
wasim-redalkemi <wasim.redalkemi@gmail.com>
2022-07-06 17:00:37 +0530

    changes

--- server.js
+++ server.js
@@ -44,6 +44,43 @@ app.get(["/posts", "/categories"], (req, res) => {
   });
 });
 
+
+app.get("/generate-sitemap", (req, res) => {
+
+  const url = "https://themoviesflix.cx/wp-json/wp/v2/posts?order_by=modified&per_page=50";
+  const sitemap_url = "https://movies-king.herokuapp.com/download/";
+
+  request(url, (error, response, body) => {
+
+    var result = `
+      <?xml version="1.0" encoding="UTF-8" ?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+        http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    `;
+
+    JSON.parse(body).map((item, index) => {
+      result += `
+        <url>
+          <loc>${sitemap_url + item.slug}</loc>
+        </url>
+      `;
+    });
+
+    result += `</urlset>`; 
+    //disable cache
+    res.set({
+      'Cache-control' : 'no-cache, no-store max-age=0',
+      'Content-Disposition' : `attachment; filename=sitemap_posts.xml`
+    });
+    res.send(result.trim());
+
+  });
+  
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
